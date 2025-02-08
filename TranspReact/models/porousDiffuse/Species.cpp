c03e48e2eec0a68e880ffb91aa68c27a449a4b3b
hsitaram <hariswaran@gmail.com>
2025-02-07 17:16:31 -0700

    adding porous diffuse case with pine box

---
+++ TranspReact/models/porousDiffuse/Species.cpp
@@ -0,0 +1,26 @@
+#include<species.H>
+
+namespace tr_species
+{
+    amrex::Vector<std::string> specnames(NUM_SPECIES);
+
+    void init()
+    {
+        specnames[SOLID_ID]="solid";
+        specnames[TEMP_ID]="Temperature";
+    }    
+    void close()
+    {
+        specnames.clear();
+    }
+    int find_id(std::string specname)
+    {
+        int loc=-1;
+        auto it=std::find(specnames.begin(),specnames.end(),specname);
+        if(it != specnames.end())
+        {
+            loc=it-specnames.begin();
+        }
+        return(loc);
+    }
+}
