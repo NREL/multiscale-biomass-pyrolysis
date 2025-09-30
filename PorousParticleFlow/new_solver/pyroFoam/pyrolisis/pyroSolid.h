54486a64447e1a2439444c43482eae00357370d6
Federico Municchi <federico.municchi@gmail.com>
2025-09-30 10:18:39 -0600

    Added density to pyrosolid

--- PorousParticleFlow/new_solver/pyroFoam/pyrolisis/pyroSolid.h
+++ PorousParticleFlow/new_solver/pyroFoam/pyrolisis/pyroSolid.h
@@ -15,6 +15,7 @@ private:
   const fvMesh &m_mesh;
   IOdictionary m_dict;
   volScalarField m_porosity;
+  volScalarField m_rho;
   volScalarField m_T;
   volScalarField m_rhoCp;
  // volTensorField m_Kp;
